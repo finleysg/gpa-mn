9105d55b83688cb687fe61b73fcf5f2a1b195203
Stuart Finley <finleysg@gmail.com>
2026-04-11 11:09:06 -0500

    chore(database): remove all migrations - totally borked

--- packages/database/drizzle/0003_add_user_phone.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `user` ADD `phone` varchar(32);
--- packages/database/drizzle/0004_add_notification_preferences.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `user` ADD `notifyOnSubmission` boolean NOT NULL DEFAULT true;
-ALTER TABLE `user` ADD `notifyOnAssignment` boolean NOT NULL DEFAULT true;
